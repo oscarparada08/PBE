158abb8e7c63c77fecb6be7a850e06d1fdb2d2ec
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-10-07 23:30:38 +0200

    PUZZLE1.rb

--- PUZZLE1.rb
+++ PUZZLE1.rb
@@ -1,67 +1,69 @@
-"use strict";
-const Mfrc522 = require("./../index");
-const SoftSPI = require("rpi-softspi");
+# Este script utiliza un lector RFID MFRC522 para leer el UID de tarjetas RFID
+# Requiere una Raspberry Pi y módulos adicionales para GPIO y control de colores en terminal.
 
-//# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-console.log("scanning...");
-console.log("Please put chip or keycard in the antenna inductive zone!");
-console.log("Press Ctrl-C to stop.");
+require 'rpi_gpio'  # Biblioteca para controlar los pines GPIO en la Raspberry Pi
+require 'colorize'  # Biblioteca para agregar color al texto en la terminal
+require 'mfrc522'   # Biblioteca para interactuar con el lector RFID MFRC522
 
-const softSPI = new SoftSPI({
-  clock: 23, // pin number of SCLK
-  mosi: 19, // pin number of MOSI
-  miso: 21, // pin number of MISO
-  client: 24 // pin number of CS
-});
+# Definimos una clase que se encargará de manejar el lector RFID
+class RfidRc522
+  # Método que inicializa el lector, lee el UID de la tarjeta y lo devuelve en formato hexadecimal
+  def scan_uid
+    # Creamos una nueva instancia del lector RFID
+    reader = MFRC522::Reader.new
+    
+    # Leemos el UID de la tarjeta RFID
+    uid = reader.read_uid
+    
+    # Convertimos el UID a formato hexadecimal en mayúsculas
+    uid_hex = uid.map { |byte| byte.to_s(16).upcase }.join
+    
+    # Retornamos el UID en formato hexadecimal
+    return uid_hex
+  end
+end
 
-// GPIO 24 can be used for buzzer bin (PIN 18), Reset pin is (PIN 22).
-// I believe that channing pattern is better for configuring pins which are optional methods to use.
-const mfrc522 = new Mfrc522(softSPI).setResetPin(22).setBuzzerPin(18);
+# Método que limpia la pantalla del terminal
+def clear_screen
+  system('clear')
+end
 
-setInterval(function() {
-  //# reset card
-  mfrc522.reset();
+# Variable para controlar el ciclo de escaneo
+opc = ""
 
-  //# Scan for cards
-  let response = mfrc522.findCard();
-  if (!response.status) {
-    console.log("No Card");
-    return;
-  }
-  console.log("Card detected, CardType: " + response.bitSize);
+# Bucle que sigue ejecutándose hasta que el usuario elija no escanear más
+while opc != "n"
+  # Limpiamos la pantalla antes de cada escaneo
+  clear_screen
+  
+  # Mostramos un mensaje con instrucciones para el usuario, con colores
+  puts "\t" + "<<<<<<<<<<<<".red
+  puts "\t" + "    SCAN   ".yellow
+  puts "\t" + "    YOUR   ".yellow
+  puts "\t" + "    PASS   ".yellow
+  puts "\t" + "<<<<<<<<<<<<".red
 
-  //# Get the UID of the card
-  response = mfrc522.getUid();
-  if (!response.status) {
-    console.log("UID Scan Error");
-    return;
-  }
-  //# If we have the UID, continue
-  const uid = response.data;
-  console.log(
-    "Card read UID: %s %s %s %s",
-    uid[0].toString(16),
-    uid[1].toString(16),
-    uid[2].toString(16),
-    uid[3].toString(16)
-  );
-
-  //# Select the scanned card
-  const memoryCapacity = mfrc522.selectCard(uid);
-  console.log("Card Memory Capacity: " + memoryCapacity);
-
-  //# This is the default key for authentication
-  const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-
-  //# Authenticate on Block 8 with key and uid
-  if (!mfrc522.authenticate(8, key, uid)) {
-    console.log("Authentication Error");
-    return;
-  }
-
-  //# Dump Block 8
-  console.log("Block: 8 Data: " + mfrc522.getDataForBlock(8));
-
-  //# Stop
-  mfrc522.stopCrypto();
-}, 500);
+  begin
+    # Inicializamos el objeto para manejar el lector RFID
+    rf = RfidRc522.new
+    
+    # Escaneamos y obtenemos el UID de la tarjeta
+    uid = rf.scan_uid
+    
+    # Mostramos el UID obtenido en la terminal
+    puts "\t YOUR UID IS:"
+    puts "\t" + ">>>>>>>>>>".green
+    puts "\t" + uid.strip.sub(/^0x/i, "").green  # Eliminamos el prefijo '0x' y mostramos en verde
+    puts "\t" + ">>>>>>>>>>".green
+  ensure
+    # Preguntamos al usuario si quiere escanear otra tarjeta
+    opc = "n"  # Inicializamos la opción como 'n' (para evitar que se quede en ciclo infinito)
+    print "\t SCAN AGAIN? (y/n): "
+    
+    # Leemos la entrada del usuario y la convertimos a minúsculas
+    opc = gets.chomp.downcase
+    
+    # Limpiamos los pines GPIO después de cada escaneo
+    RPi::GPIO.clean_up
+  end
+end
