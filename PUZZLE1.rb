7721376e6d30b63ab084113086de9390d9ecadcc
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-10-08 01:33:23 +0200

    PUZZLE1.rb

--- PUZZLE1.rb
+++ PUZZLE1.rb
@@ -23,25 +23,22 @@ while opc != "n"
   puts "\t" + "<<<<<<<<<<<<".red
 
   begin
-    # Creamos una nueva instancia del lector RFID
-    reader = MFRC522::Reader.new
-
     # Busca nuevas tarjetas
-    unless reader.picc_is_new_card_present
+    if !MFRC522::Reader.picc_is_new_card_present
       puts "No card present. Please try again.".red
       sleep(1)
       next  # Salta a la siguiente iteración si no hay tarjeta presente
     end
 
     # Lee la tarjeta
-    unless reader.picc_read_card_serial
+    if !MFRC522::Reader.picc_read_card_serial
       puts "Failed to read card. Please try again.".red
       sleep(1)
       next  # Salta a la siguiente iteración si no se puede leer la tarjeta
     end
 
     # Convertimos el UID a formato hexadecimal
-    uid_hex = reader.uid.map { |byte| byte.to_s(16).upcase.rjust(2, '0') }.join
+    uid_hex = MFRC522::Reader.uid.map { |byte| byte.to_s(16).upcase.rjust(2, '0') }.join
     puts "\t YOUR UID IS:"
     puts "\t" + ">>>>>>>>>>".green
     puts "\t" + uid_hex.green  # Mostramos el UID en verde
@@ -58,3 +55,5 @@ while opc != "n"
   opc = gets.chomp.downcase
 end
 
+# Limpiamos los pines GPIO después de cada escaneo
+PiPiper.clean_up  # Limpiamos los pines GPIO
