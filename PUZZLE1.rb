43d939aa0b88bb879800dfb96e4d9ae29a2bda86
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-10-07 22:29:53 +0200

    PUZZLE1.rb

--- PUZZLE1.rb
+++ PUZZLE1.rb
@@ -1,38 +1,66 @@
-require 'gpio'
-require 'mrfc522'
-require 'colorize'
+require 'pigpio'
 
-# Inicializar el lector RC522
-reader = MRFC522.new(
-  sda: 8,   # Pin SDA
-  reset: 7  # Pin Reset
-)
+# Configura los pines
+SS_PIN = 8   # CE0
+RST_PIN = 25
 
-# Función para escanear el RFID
-def scan_rfid(reader)
-  puts "Acerque su tarjeta RFID..."
-  uid = reader.read_uid
-  if uid
-    puts "¡Tarjeta detectada! UID: #{uid.join('-').colorize(:green)}"
-    uid.join('-')  # Devuelve el UID como una cadena
-  else
-    puts "No se detectó ninguna tarjeta.".colorize(:red)
-    nil
-  end
+# Inicia la biblioteca pigpio
+$gpio = Pigpio::Gpio.new
+
+# Configura los pines
+$gpio.set_mode(SS_PIN, Pigpio::OUTPUT)
+$gpio.set_mode(RST_PIN, Pigpio::OUTPUT)
+
+# Función para escribir un valor en el registro
+def rc522_write(register, value)
+  $gpio.write(RST_PIN, 0)  # Activar el módulo
+  # Aquí se realizaría la comunicación SPI para escribir el valor
+  $gpio.write(RST_PIN, 1)  # Desactivar el módulo
 end
 
-loop do
-  # Escanear la tarjeta
-  scanned_uid = scan_rfid(reader)
-
-  # Preguntar si quiere volver a escanear
-  if scanned_uid
-    print "¿Desea escanear otra tarjeta? (s/n): "
-    respuesta = gets.chomp.downcase
-    break unless respuesta == 's'
-  else
-    puts "Intente nuevamente.".colorize(:yellow)
-  end
+# Función para leer un valor del registro
+def rc522_read(register)
+  $gpio.write(RST_PIN, 0)  # Activar el módulo
+  # Aquí se realizaría la comunicación SPI para leer el valor
+  $gpio.write(RST_PIN, 1)  # Desactivar el módulo
+  return 0  # Devuelve un valor de prueba
 end
 
-puts "Proceso finalizado.".colorize(:blue)
+# Inicializa el módulo RC522
+def init_rc522
+  rc522_write(0x01, 0x0F)  # Activar el módulo
+  rc522_write(0x2A, 0x8D)  # Configurar el modo de espera
+end
+
+# Función para detectar la tarjeta
+def detect_card
+  # Implementa la lógica para detectar la tarjeta
+  return true  # Para pruebas, devuelve verdadero
+end
+
+# Función para leer el ID de la tarjeta
+def read_card_id
+  return "123456"  # Para pruebas, devuelve un ID fijo
+end
+
+# Bucle principal
+begin
+  init_rc522
+  puts "Esperando a detectar una tarjeta..."
+
+  loop do
+    if detect_card
+      puts "Tarjeta detectada!"
+      card_id = read_card_id
+      puts "ID de la tarjeta: #{card_id}"
+      sleep 1  # Espera antes de volver a detectar
+    end
+    sleep 0.1  # Pequeña pausa para evitar sobrecarga
+  end
+
+rescue Interrupt
+  puts "Saliendo..."
+ensure
+  $gpio.write(RST_PIN, 1)  # Asegúrate de desactivar el módulo
+  $gpio.stop
+end
