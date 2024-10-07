575da99f08a06ee9fa494d5cb317fadf1cfc8cde
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-10-08 01:13:00 +0200

    PUZZLE1.rb

--- PUZZLE1.rb
+++ PUZZLE1.rb
@@ -1,22 +1,21 @@
-# Este script utiliza un lector RFID MFRC522 para leer el UID de tarjetas RFID
-# Requiere una Raspberry Pi y módulos adicionales para GPIO y control de colores en terminal.
-
-require 'pi_piper' # Biblioteca para controlar los pines GPIO en la Raspberry Pi
-require 'colorize'  # Biblioteca para agregar color al texto en la terminal
-require 'mfrc522'   # Biblioteca para interactuar con el lector RFID MFRC522
+require 'pi_piper'  # Biblioteca para controlar los pines GPIO en la Raspberry Pi
+require 'colorize'   # Biblioteca para agregar color al texto en la terminal
+require 'mfrc522'    # Biblioteca para interactuar con el lector RFID MFRC522
 
 # Definimos una clase que se encargará de manejar el lector RFID
 class RfidRc522
-  def initialize
-    @reader = MFRC522::Reader.new  # Inicializamos el lector RFID
-  end
-
-  # Método que lee el UID de la tarjeta y lo devuelve en formato hexadecimal
+  # Método que inicializa el lector, lee el UID de la tarjeta y lo devuelve en formato hexadecimal
   def scan_uid
-    uid = @reader.read_uid  # Leemos el UID de la tarjeta RFID
-
+    # Creamos una nueva instancia del lector RFID
+    reader = MFRC522::Reader.new
+    
+    # Leemos el UID de la tarjeta RFID
+    uid = reader.read_uid
+    
     # Convertimos el UID a formato hexadecimal en mayúsculas
     uid_hex = uid.map { |byte| byte.to_s(16).upcase }.join
+    
+    # Retornamos el UID en formato hexadecimal
     return uid_hex
   end
 end
@@ -31,8 +30,9 @@ opc = ""
 
 # Bucle que sigue ejecutándose hasta que el usuario elija no escanear más
 while opc != "n"
-  clear_screen  # Limpiamos la pantalla antes de cada escaneo
-
+  # Limpiamos la pantalla antes de cada escaneo
+  clear_screen
+  
   # Mostramos un mensaje con instrucciones para el usuario, con colores
   puts "\t" + "<<<<<<<<<<<<".red
   puts "\t" + "    SCAN   ".yellow
@@ -46,7 +46,7 @@ while opc != "n"
     
     # Escaneamos y obtenemos el UID de la tarjeta
     uid = rf.scan_uid
-
+    
     # Mostramos el UID obtenido en la terminal
     puts "\t YOUR UID IS:"
     puts "\t" + ">>>>>>>>>>".green
@@ -60,8 +60,8 @@ while opc != "n"
     
     # Leemos la entrada del usuario y la convertimos a minúsculas
     opc = gets.chomp.downcase
-
+    
     # Limpiamos los pines GPIO después de cada escaneo
-    PiPiper.clean_up
+    PiPiper.clean_up  # Limpiar pines GPIO
   end
 end
