74afef038a4cd04c08eb64a47b5ca3ea3bc7d3d7
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-11-10 17:23:20 +0100

    Delete test_gtk3.rb

--- test_gtk3.rb
+++
@@ -1,9 +0,0 @@
-require 'gtk3'
-
-window = Gtk::Window.new
-window.set_title("GTK3 Test")
-window.set_default_size(200, 100)
-window.signal_connect("destroy") { Gtk.main_quit }
-
-window.show_all
-Gtk.main
