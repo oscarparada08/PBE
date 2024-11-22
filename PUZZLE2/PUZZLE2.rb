24a75711bf31342bd288f5a3a8e655107f7bbaec
oscarparada08 <oscar.parada@estudiantat.upc.edu>
2024-11-22 23:20:21 +0100

    PUZZLE2.rb

--- PUZZLE2/PUZZLE2.rb
+++ PUZZLE2/PUZZLE2.rb
@@ -1,43 +1,68 @@
 require "gtk3"
-require_relative 'PUZZLE1'
-require_relative 'widget_options'
+require "thread"
+require_relative "PUZZLE1.rb"
 
-rf = Rfid.new                                                         #Objecte únic RFID
+class Window < Gtk::Window
+  def initialize
+    super
+    set_title 'rfid_gtk.rb' 
+    set_border_width 10
+    set_size_request 500, 200
 
-def scan_tag(rf, info_label)
-  thr = Thread.new {                                                  #Fil auxiliar bloquejant per llegir RFID
-    uid =  rf.read_uid                                                #Lectura 
-    puts "Tag detected: " + uid                                       #Mostrem UID per command-prompt.
-    info_label.set_markup("uid: " + uid)                              #Mostrem UID per pantalla.
-    info_label.override_background_color(0, Gdk::RGBA::new(1,0,0,1))  #Modificació a color vermell de l'etiqueta.
-  }
+    # Load CSS
+    load_css("style.css")
+
+    signal_connect("destroy") do
+      Gtk.main_quit
+      @thread.kill if @thread
+    end
+
+    @hbox = Gtk::Box.new(:vertical, 6)
+    add(@hbox)
+
+    # Label
+    @label = Gtk::Label.new("Please, login with your university card")
+    @label.set_name("label") # Apply CSS ID
+    @hbox.pack_start(@label)
+
+    # Button
+    @button = Gtk::Button.new(label: 'Clear')
+    @button.set_name("button") # Apply CSS ID
+    @button.signal_connect('clicked') { on_clear_clicked }
+    @hbox.pack_start(@button)
+  end
+
+  def on_clear_clicked
+    @label.set_markup("Please, login with your university card")
+    @label.set_name("label") # Reset CSS class
+    @thread.kill if @thread
+    rfid
+  end
+
+  def rfid
+    @rfid = Rfid.new
+    @thread = Thread.new do
+      uid = @rfid.read_uid
+      GLib::Idle.add do
+        @label.set_markup("uid: #{uid}")
+        @label.set_name("label red") # Change CSS class
+        false
+      end
+    end
+  end
+
+  def load_css(file)
+    provider = Gtk::CssProvider.new
+    provider.load(path: file)
+    Gtk::StyleContext.add_provider_for_screen(
+      Gdk::Screen.default,
+      provider,
+      Gtk::StyleProvider::PRIORITY_USER
+    )
+  end
 end
 
-#S'ha utilitzat el fitxer widget_options.rb, on tenim, de manera separada, 
-#les variables de configuració per una manipulació més senzilla.
-
-window = get_window		#Finestra. Objecte gràfic que encapsula tots els objectes gràfics
-grid = get_grid			#Graella. Utilitat per organitzar objectes a la finestra
-info_label = get_label		#Etiqueta. Canvia de color i text segons estat de l'aplicació 
-clear_button = get_button	#Botó 'clear' (per tornar a llegir RFID)
-
-#Afegir objectes a graella, determinant la seva posició.
-grid.attach(info_label,0,0,1,1)
-grid.attach(clear_button,0,1,1,1)
-window.set_window_position(:center) #Pantalla al centre
-
-
-clear_button.signal_connect("clicked") do #Actuació en cas de accionar botó
-  reset_label(info_label)                 #Reestabliment blau i missatge
-  scan_tag(rf, info_label)                
-end
-
-window.signal_connect('destroy') { Gtk.main_quit } #Botó de tancament pantalla gràfica, finalitza aplicació
-
-
-# Run Application
-window.add(grid) #Afegim graella amb etiqueta i botó a la finestra visible
-window.show_all  #Mostrem elements 
-scan_tag(rf, info_label) #Invoquem mètode d'escanejament per primer cop
-
-Gtk.main                  #Bucle gràfic
+win = Window.new
+win.show_all
+win.rfid
+Gtk.main
